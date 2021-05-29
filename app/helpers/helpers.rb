d0cfa85641c7f98029a3d577f4bd7886f2839965
Muhammad <muhammadhullah23@gmail.com>
2021-05-29 18:52:47 -0400

    commit tree 27e862b49a6f67d394280735a0176712e6ea6aeb

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,9 @@
 class Helpers
 
-    def self.current_user(session_hash)
-        @user = User.find_by_id(session[:user_id])
-        @user
+    def self.current_user(session)
+        User.find_by_id(session[:user_id])
     end
-    def self.is_logged_in?(session_hash)
+    def self.is_logged_in?(session)
         !!session[:user_id]
     end
 end
